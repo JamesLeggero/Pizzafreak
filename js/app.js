3ad4f188c1b35c16e7d346965b4f11e5464ef5f0
jamesleggero <james.m.leggero@gmail.com>
2020-08-01 14:28:54 -0400

    gameAsset refac

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 console.log($)
 
-$( ()=>{
+// $( ()=>{
 
-    class Customer {
+
+// });
+
+
+const gameAssets = {   
+    customer: class Customer {
         constructor(name) {
             this.name = name;
             this.favToppings = [];
@@ -11,56 +16,47 @@ $( ()=>{
         }
         sayHello() {
             console.log(`Hello, I\'m ${this.name}`);
-    
         }
-    }
-    class Topping {
-        constructor(name, isMeat) {
-            this.name = name;
-            this.isMeat = isMeat;
-            this.onPlayerPizza = false;
-        }
-    }
-    class Player {
+
+    },
+    player: class Player {
         constructor(name) {
             this.name = name;
             this.money = 0;
             this.constructedPizza = []
         }
-    }
+    },
+    topping: class Topping {
+        constructor(name, isMeat) {
+            this.name = name;
+            this.isMeat = isMeat;
+            this.onPlayerPizza = false;
+        }
+    },
+}
+const ruthie = new gameAssets.customer('Ruthie')
+const kathleen = new gameAssets.customer('Kathleen')
+const chris = new gameAssets.customer('Chris')
+const greg = new gameAssets.customer('Greg')
+const margaret = new gameAssets.customer('Margaret')
+const michelle = new gameAssets.customer('Michelle')
 
-    //Customers
-    const ruthie = new Customer('Ruthie');
-    const kathleen = new Customer('Kathleen');
-    const chris = new Customer('Chris');
-    const greg = new Customer('Greg');
-    const margaret = new Customer('Margaret');
-    const michelle = new Customer('Michelle');
+const player1 = new gameAssets.player('Player 1')
+const player2 = new gameAssets.player('Player 2')
 
-    //Toppings
-    const onions = new Topping('onions', false);
-    const hots = new Topping('hot peppers', false);
-    const mushrooms = new Topping('mushrooms', false)
-    const tomatoes = new Topping('tomatoes', false)
-    const pineapples = new Topping('pineapple', false)
-    const pepperoni = new Topping('pepperoni', true)
-    const sausage = new Topping ('sausage', true)
-    const ham = new Topping('ham', true)
-    const bacon = new Topping('bacon', true)
-    const chicken = new Topping('chicken', true)
+const onions = new gameAssets.topping('onions', false);
+const hots = new gameAssets.topping('hot peppers', false);
+const mushrooms = new gameAssets.topping('mushrooms', false);
+const broccoli = new gameAssets.topping('broccoli', false);
+const pineapple = new gameAssets.topping('pineapples', false);
+const pepperoni = new gameAssets.topping('pepperoni', true);
+const sausage = new gameAssets.topping('sausage', true);
+const ham = new gameAssets.topping('ham', true);
+const bacon = new gameAssets.topping('bacon', true);
+const chicken = new gameAssets.topping('chicken', true);
 
-    const toppingsList = [onions, hots, mushrooms, tomatoes, pineapples, pepperoni, sausage, ham, bacon, chicken]
-
-    //Players (for now, limited to 2)
-    //Stretch = add an input/function that asks for player's name, then creates the new player)
-    const player1 = new Player('Player 1');
-    const player2 = new Player('Player 2')
+const toppingsList = [onions, hots, mushrooms, broccoli, pineapple, pepperoni, sausage, ham, bacon, chicken]
 
 
-    // console.log(ruthie)
+console.log(ruthie.sayHello())
 
-});
-
-// const add = (num1, num2) => {
-//     return num1 + num2
-// }
