4a3ff83891ebc8491b551f09ccc371cdaf78b0eb
jamesleggero <james.m.leggero@gmail.com>
2020-08-02 17:05:06 -0400

    Alpha version working with all functions. Just window and console for now

--- js/app.js
+++ js/app.js
@@ -21,10 +21,12 @@ const gameLogic = {
             if (player.constructedPizza.length === 0) {
                 window.alert("Let's put on some toppings!")
             }
-            let input = window.prompt(`What would you like to put on the pizza? Type: 1 for ${toppingsList[0].name}, 2 for ${toppingsList[1].name}, 3 for ${toppingsList[2].name}, 4 for ${toppingsList[3].name}, 5 for ${toppingsList[4].name}, 6 for ${toppingsList[5].name}, 7 for ${toppingsList[6].name}, 8 for ${toppingsList[7].name}, 9 for ${toppingsList[8].name}, or 10 for ${toppingsList[9].name},`)
+            let randomTopping = gameLogic.randGen(toppingsList.length);
+            randomTopping++;
+            let input = window.prompt(`What would you like to put on the pizza? Type: 1 for ${toppingsList[0].name}, 2 for ${toppingsList[1].name}, 3 for ${toppingsList[2].name}, 4 for ${toppingsList[3].name}, 5 for ${toppingsList[4].name}, 6 for ${toppingsList[5].name}, 7 for ${toppingsList[6].name}, 8 for ${toppingsList[7].name}, 9 for ${toppingsList[8].name}, or 10 for ${toppingsList[9].name}.`, randomTopping)
             input--;
             if (toppingsList[input].onPlayerPizza === true) {
-                window.alert(`${toppingsList[input].name} is already on your pizza`)
+                window.alert(`Whoops, there's already ${toppingsList[input].name} on your pizza.`)
             } else {
                 toppingsList[input].onPlayerPizza = true;
                 player.constructedPizza.push(toppingsList[input])
@@ -41,7 +43,13 @@ const gameLogic = {
         }
         window.alert('Ok! Time to see how you did!')
     },
-
+    askForPizza: (customer, player)=>{
+        if (day === 0) {
+            console.log(`Hi ${player.name}! My name is ${customer.name}. Can you please make me a pizza with ${customer.favToppings[0].name}, ${customer.favToppings[1].name}, ${customer.favToppings[2].name}, and ${customer.favToppings[3].name}? Thanks!`)
+        } else {
+            console.log(`Hey ${player.name}, it's me, ${customer.name}! Just the usual, please.`)
+        }
+    },
     comparePizzas: (customer, player) => {
 
         //making and sorting two lists based on the names of the customer toppings and player toppings
@@ -58,7 +66,7 @@ const gameLogic = {
             playerConstructedList.push(playerConstructedObjArr[i].name)
         }
     
-        console.log(customerToppingList, playerConstructedList)
+        // console.log(customerToppingList, playerConstructedList)
 
         //making the difference lists and evaluating them
 
@@ -66,8 +74,8 @@ const gameLogic = {
         const $whatPlayerMade = $(playerConstructedList).not(customerToppingList)
 
 
-        console.log($whatCustomerWanted)
-        console.log($whatPlayerMade)
+        // console.log($whatCustomerWanted)
+        // console.log($whatPlayerMade)
         
         if ($whatCustomerWanted.length > 2) {
             console.log('You really screwed up my order. I want a refund!')
@@ -81,32 +89,82 @@ const gameLogic = {
             console.log("Nice job... the perfect pizza!")
                 gameLogic.tipPlayer(player, 5)
         }
+        if(player.money < 0) {
+            player.autoLoss = true
+        }
         
-        console.log('Player money :', player.money)
-        console.log($whatCustomerWanted, $whatPlayerMade)
+        // console.log('Player money :', player.money)
+        // console.log($whatCustomerWanted, $whatPlayerMade)
     },
     checkAutoLoss: ()=>{
         if (player1.money < 0 && player2.money < 0){
             console.log('You two are a disgrace! Even my cat makes better pizzas than you. Both of you, vamoose!')
-            setTimeout(()=>{window.alert('Yikes! You both lost. Better luck next time.')}, 3000)
+            setTimeout(()=>{window.alert('Yikes! You both lost. Better luck next time.')}, 3000);
+            return 1
         } else if (player1.money < 0 || player2.money < 0){
             if (player1.money < 0) {
                 console.log(`${player1.name}, you're outta here! You're costing me way too much money! ${player2.name}, you're hired!`)
-                setTimeout(()=>{window.alert(`Great job, ${player2.name}! Enjoy your win with a delicious pizza.`)}, 3000)
+                setTimeout(()=>{window.alert(`Great job, ${player2.name}! Enjoy your win with a delicious pizza.`)}, 3000);
+                return 1
             } else {
                 console.log(`${player2.name}, get lost! You're makin' me go into debt. ${player1.name}, you're hired!`)
-                setTimeout(()=>{window.alert(`Congrats, ${player1.name}! You're the pizza champ.`)}, 3000)
+                setTimeout(()=>{window.alert(`Congrats, ${player1.name}! You're the pizza champ.`)}, 3000);
+                return 1
             }
         }
         
     },
-    askForPizza: (customer, player)=>{
-        if (day === 0) {
-            console.log(`Hi ${player.name}! My name is ${customer.name}. Can you please make me a pizza with ${customer.favToppings[0].name}, ${customer.favToppings[1].name}, ${customer.favToppings[2].name}, and ${customer.favToppings[3].name}? Thanks!`)
+    checkWinner: ()=>{
+        if (player1.money === player2.money) {
+            console.log('You two are equally good... for a coupla punks. Aw, what the heck... you\'re both hired!')
+        } else if (player1.money > player2.money) {
+            console.log(`${player1.name}, I think we can all see you're top pizzamaker. ${player2.name}, scram!`)
         } else {
-            console.log(`Hey ${player.name}, it's me, ${customer.name}! Just the usual, please.`)
+            console.log(`${player2.name}, the pizza whisperer. Come back new week!`)
         }
+        return;
+    },
+    runDayLoop: ()=>{
+        if (day === 0) {
+            for (let i = 0; i < 3; i++) {
+                gameLogic.askForPizza(fullCustomerList[i], player1)
+            }
+            for (let i = 3; i < 6; i++) {
+                gameLogic.askForPizza(fullCustomerList[i], player2)
+            }
+            day++;
+        } else {
+            window.alert(`It's Day ${day}. Let's make some freakin' pizza!!`)
+            const todaysCustomerP1 = fullCustomerList[gameLogic.randGen(fullCustomerList.length)];
+            const todaysCustomerP2 = fullCustomerList[gameLogic.randGen(fullCustomerList.length)];
+            gameLogic.askForPizza(todaysCustomerP1, player1);
+            gameLogic.playerMakesPizza(player1)
+            gameLogic.comparePizzas(todaysCustomerP1, player1);
+            gameLogic.askForPizza(todaysCustomerP2, player2);
+            gameLogic.playerMakesPizza(player2)
+            gameLogic.comparePizzas(todaysCustomerP2, player2);
+            window.alert(`Long night! ${player1.name} has $${player1.money} and ${player2.name} has $${player2.money}.`);
+            day++;
+        }
+    },
+    fullGame: ()=>{
+        let input = window.prompt('Welcome to Pizzafreak! How many days do you want to compete?', '3')
+        const p1Name = window.prompt('Please type Player 1\'s name', 'Player 1');
+        const p2Name = window.prompt('Please type Player 2\'s name', 'Player 2')
+        player1.name = p1Name;
+        player2.name = p2Name;
+        input++;
+        while (day < input) {
+            gameLogic.runDayLoop()
+            if (gameLogic.checkAutoLoss() === 1) {
+                return;
+            };
+        }
+        gameLogic.checkWinner()
     }
+    
+
+
 }
 
 const gameAssets = {
@@ -177,17 +235,22 @@ const michelle = new gameAssets.customer('Michelle')
 const fullCustomerList = [ruthie, kathleen, chris, greg, margaret, michelle]
 
 const player1 = new gameAssets.player('Player 1')
-player1.constructedPizza.push(toppingsList[gameLogic.randGen(toppingsList.length)])
-player1.constructedPizza.push(toppingsList[gameLogic.randGen(toppingsList.length)])
-player1.constructedPizza.push(toppingsList[gameLogic.randGen(toppingsList.length)])
-player1.constructedPizza.push(toppingsList[gameLogic.randGen(toppingsList.length)])
+// player1.constructedPizza.push(toppingsList[gameLogic.randGen(toppingsList.length)])
+// player1.constructedPizza.push(toppingsList[gameLogic.randGen(toppingsList.length)])
+// player1.constructedPizza.push(toppingsList[gameLogic.randGen(toppingsList.length)])
+// player1.constructedPizza.push(toppingsList[gameLogic.randGen(toppingsList.length)])
 const player2 = new gameAssets.player('Player 2')
 
-console.log(ruthie)
-console.log(greg)
-console.log(toppingsList)
-console.log(player1)
+// console.log(ruthie)
+// console.log(greg)
+// console.log(toppingsList)
+// console.log(player1)
 
+// gameLogic.fullGame();
+
+console.log('Please type the following to play:')
+console.log('gameLogic.fullGame()')
+console.log('Reload (Command-R) when the game is over to play again')
 
 $( ()=>{
     
